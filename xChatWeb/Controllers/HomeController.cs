59d22bfdc680374c74a95aee72b50951cf32fdea
whuaman <whuaman@xirectss.com>
2019-04-03 17:52:57 -0500

    Consumo de Servicio de Lista

--- xChatWeb/Controllers/HomeController.cs
+++ xChatWeb/Controllers/HomeController.cs
@@ -11,8 +11,52 @@ namespace xChatWeb.Controllers
         {
             ViewBag.error = "";
 
+            ListUserConnect listUserConnect = new ListUserConnect
+            {
+                Elementos = new System.Collections.Generic.List<UserConnect>
+            {
+                new UserConnect()
+                {
+                    ChatId = 1,
+                    UserEmail = "correo1@correo.com",
+                    UserName = "Anonimo 01",
+                    UserToken = "token12345678"
+                },
 
+                new UserConnect()
+                {
+                    ChatId = 1,
+                    UserEmail = "correo2@correo.com",
+                    UserName = "Anonimo 02",
+                    UserToken = "token12345678"
+                },
 
+                new UserConnect()
+                {
+                    ChatId = 1,
+                    UserEmail = "correo3@correo.com",
+                    UserName = "Anonimo 03",
+                    UserToken = "token12345678"
+                },
+
+                new UserConnect()
+                {
+                    ChatId = 1,
+                    UserEmail = "correo4@correo.com",
+                    UserName = "Anonimo 04",
+                    UserToken = "token12345678"
+                },
+
+                new UserConnect()
+                {
+                    ChatId = 1,
+                    UserEmail = "correo5@correo.com",
+                    UserName = "Anonimo 05",
+                    UserToken = "token12345678"
+                }
+            }
+            };
+            ViewBag.UserActive = listUserConnect;
             return View();
         }
 
