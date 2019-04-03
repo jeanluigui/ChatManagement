ce0b1212cb26d844aa9d48f69755ac1021cf4e79
jhurtado <jhurtado@xirectss.com>
2019-04-03 18:37:07 -0500

    list conversations

--- xChatWeb/Controllers/HomeController.cs
+++ xChatWeb/Controllers/HomeController.cs
@@ -72,64 +72,25 @@ namespace xChatWeb.Controllers
         {
             ViewBag.Message = "Your contact page.";
 
-            ObjectRequest<int> objectRequest = new ObjectRequest<int>()
-            {
-                SenderObject = 1
-            };
+            // ----------------------------------------------
+            // Obtener lista de usuarios conectados.
+            // ----------------------------------------------
+            ObjectRequest<int> objectRequest = new ObjectRequest<int>() { SenderObject = 1 };
 
-            ObjectResult<ListUserConnect> result = RequestService.Execute<ListUserConnect, int>(Constants.UrlApiService.GetListUserConnectByAccountManagerId
+            ObjectResult<ListUserConnect> lista = RequestService.Execute<ListUserConnect, int>(Constants.UrlApiService.GetListUserConnectByAccountManagerId
                 , "POST"
                 , objectRequest);
 
-            ListUserConnect lista = result.Data as ListUserConnect;
+            // ----------------------------------------------
+            // Obtener conversación.
+            // ----------------------------------------------
+            objectRequest = new ObjectRequest<int>() { SenderObject = 1 };
 
-            ListUserConnect listUserConnect = new ListUserConnect
-            {
-                Elementos = new System.Collections.Generic.List<UserConnect>
-            {
-                new UserConnect()
-                {
-                    ChatId = 1,
-                    UserEmail = "correo1@correo.com",
-                    UserName = "Anonimo 01",
-                    UserToken = "token12345678"
-                },
+            ObjectResult<ListConversationResponseEntity> listConversations = RequestService.Execute<ListConversationResponseEntity, int>(Constants.UrlApiService.GetListConversationByChatId
+                , "POST"
+                , objectRequest);
 
-                new UserConnect()
-                {
-                    ChatId = 1,
-                    UserEmail = "correo2@correo.com",
-                    UserName = "Anonimo 02",
-                    UserToken = "token12345678"
-                },
-
-                new UserConnect()
-                {
-                    ChatId = 1,
-                    UserEmail = "correo3@correo.com",
-                    UserName = "Anonimo 03",
-                    UserToken = "token12345678"
-                },
-
-                new UserConnect()
-                {
-                    ChatId = 1,
-                    UserEmail = "correo4@correo.com",
-                    UserName = "Anonimo 04",
-                    UserToken = "token12345678"
-                },
-
-                new UserConnect()
-                {
-                    ChatId = 1,
-                    UserEmail = "correo5@correo.com",
-                    UserName = "Anonimo 05",
-                    UserToken = "token12345678"
-                }
-            }
-            };
-
-            return View(listUserConnect);
+            return View(lista);
         }
     }
 }
